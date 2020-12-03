00f95d7b8d4ad6aecd0b647902f181f23ac15acc
DeboDevelop <debajyotidofficial@gmail.com>
2020-12-03 15:12:04 +0530

    Created the VueJS Project and created and added the API key.

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+<div id="app"></div>
+</template>
+
+<script>
+require("dotenv").config();
+
+export default {
+  name: "App",
+    data () {
+    return {
+      api_key: process.env.API_KEY ,
+    }
+  },
+};
+</script>
+
+<style>
+
+</style>
