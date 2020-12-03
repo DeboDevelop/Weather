822dceeb1634b86b232db8b5947ff9c158a2fe7b
DeboDevelop <debajyotidofficial@gmail.com>
2020-12-03 16:18:55 +0530

    Fetching the data from the api and displaying it. Also fixed a error with environment variable.

--- src/App.vue
+++ src/App.vue
@@ -10,13 +10,15 @@
           @keypress="fetchWeather"
         />
       </div>
-      <div class="location-box">
-        <div class="location">Kolkata, India</div>
-        <div class="date">3, December 2020</div>
-      </div>
-      <div class="weather-box">
-        <div class="temp">10°c</div>
-        <div class="weather">Sunny</div>
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+        <div class="location-box">
+          <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
+          <div class="date">3, December 2020</div>
+        </div>
+        <div class="weather-box">
+          <div class="temp">10°c</div>
+          <div class="weather">Sunny</div>
+        </div>
       </div>
     </main>
   </div>
@@ -27,11 +29,27 @@ require("dotenv").config();
 
 export default {
   name: "App",
-    data () {
+  data () {
     return {
-      api_key: process.env.API_KEY ,
+      api_key: process.env.VUE_APP_API_KEY,
+      url_base: 'https://api.openweathermap.org/data/2.5/',
+      query: '',
+      weather: {}
     }
   },
+  methods: {
+    fetchWeather (e) {
+      if (e.key == "Enter") {
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+          .then(res => {
+            return res.json();
+          }).then(this.setResults);
+      }
+    },
+    setResults (results) {
+      this.weather = results;
+    }
+  }
 };
 </script>
 
