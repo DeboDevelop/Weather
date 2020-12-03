bff42fe036d15ab296ad89ceb8763fe5cfb4ff88
DeboDevelop <debajyotidofficial@gmail.com>
2020-12-03 15:54:08 +0530

    Created the display box to display location, weather, temperature and styled them.

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,14 @@
           @keypress="fetchWeather"
         />
       </div>
+      <div class="location-box">
+        <div class="location">Kolkata, India</div>
+        <div class="date">3, December 2020</div>
+      </div>
+      <div class="weather-box">
+        <div class="temp">10°c</div>
+        <div class="weather">Sunny</div>
+      </div>
     </main>
   </div>
 </template>
@@ -79,4 +87,44 @@ main {
   border-radius: 16px 0px 16px 0px;
 }
 
+.location-box .location {
+  color: #FFF;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+
+.location-box .date {
+  color: #FFF;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+
+.weather-box {
+  text-align: center;
+}
+
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: #FFF;
+  font-size: 102px;
+  font-weight: 900;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color:rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box .weather {
+  color: #FFF;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
 </style>
